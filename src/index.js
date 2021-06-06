ec0dd0f547ef6b9bcb2acd28a9821a97b71cf7ba
SilviaDam91 <silvia.pariota@gmail.com>
2021-06-06 19:56:45 +0200

    JS // Hourly forecast included

--- src/index.js
+++ src/index.js
@@ -132,19 +132,19 @@ function formatDayForecast(timestamp) {
 function getForecast(coordinates) {
   let oneCallApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(oneCallApiUrl).then(displayForecast);
+  axios.get(oneCallApiUrl).then(displayHourlyForecast);
 }
 
 function displayForecast(response) {
   let forecastResult = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-
   forecastResult.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
         forecastHTML +
-        `<div class="row">
-            <div class="col-6">
+        `
+            <div class="col-2">
               <div class="weather-forecast-date">${formatDayForecast(
                 forecastDay.dt
               )}</div>
@@ -164,7 +164,7 @@ function displayForecast(response) {
                 )}° </span>
               </div>
             </div>
-          </div>`;
+          `;
     }
   });
 
@@ -173,12 +173,59 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function formatHourlyForecast(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+
+  return `${hour}:${minute}`;
+}
+
+function displayHourlyForecast(response) {
+  let forecastHourlyResult = response.data.hourly;
+  let forecastHourlyElement = document.querySelector("#hourly-forecast");
+  let forecastHourlyHTML = `<div class="row">`;
+  forecastHourlyResult.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHourlyHTML =
+        forecastHourlyHTML +
+        `
+            <div class="col-2">
+              <div class="weather-forecast-date">${formatHourlyForecast(
+                forecastDay.dt
+              )}</div>
+              <img
+                src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
+                alt=""
+                width="42"
+              />
+              <div class="weather-forecast-temperatures">
+                <span> ${Math.round(forecastDay.temp)}° </span>
+                    </div>
+            </div>
+          `;
+    }
+  });
+
+  forecastHourlyHTML = forecastHourlyHTML + `</div>`;
+
+  forecastHourlyElement.innerHTML = forecastHourlyHTML;
+}
+
 currentWeatherButton.addEventListener("click", clickRetrievePosition);
 searchEngine.addEventListener("submit", handleSubmit);
-
-defaultCity("Amsterdam");
-
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-displayForecast();
+defaultCity("Amsterdam");
+getForecast();

8d38e016d860ad68e496c99d2f1a8f4598d65a7c
SilviaDam91 <silvia.pariota@gmail.com>
2021-06-06 13:16:42 +0200

    JS/ Forecast included

--- src/index.js
+++ src/index.js
@@ -122,35 +122,50 @@ function displayCelsiusTemperature(event) {
   temperatureElement.innerHTML = celsiusTemperature;
 }
 
+function formatDayForecast(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function getForecast(coordinates) {
   let oneCallApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(oneCallApiUrl).then(displayForecast);
 }
 
 function displayForecast(response) {
-  le;
-
+  let forecastResult = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  ///let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="row">
+  forecastResult.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="row">
             <div class="col-6">
-              <div class="weather-forecast-date">${day}</div>
+              <div class="weather-forecast-date">${formatDayForecast(
+                forecastDay.dt
+              )}</div>
               <img
-                src="http://openweathermap.org/img/wn/50d@2x.png"
+                src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
                 alt=""
                 width="42"
               />
               <div class="weather-forecast-temperatures">
-                <span class="max_temperature"> 18° </span>
-                <span class="min_temperature"> 12° </span>
+                <span class="max_temperature"> ${Math.round(
+                  forecastDay.temp.max
+                )}° </span>
+                <span class="min_temperature"> ${Math.round(
+                  forecastDay.temp.min
+                )}° </span>
               </div>
             </div>
           </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -165,3 +180,5 @@ defaultCity("Amsterdam");
 
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+displayForecast();
